949f84c4c6cdd3d8381c393db2662edd46c5bae4
Louis Vignoli <louis.vignoli@gmail.com>
2025-10-27 14:04:36 +0100

    Improve documentation

--- src/ziggy/__init__.py
+++ src/ziggy/__init__.py
@@ -1,10 +1,10 @@
-"""Python support for the Ziggy data serialization language.
+"""Python support for the [Ziggy](https://ziggy-lang.io) data serialization language.
 
-Leverage tree-sitter and the Ziggy tree-sitter grammar.
+Leverage tree-sitter and the [Ziggy tree-sitter grammar](https://github.com/lvignoli/tree-sitter-ziggy).
 Ziggy schema is not supported.
 
-This is work in progress, especially the public API of the package, that is a
-heterogeneous.
+This is work in progress, especially the public API of the package, that is at present
+quite heterogeneous.
 """
 
 from ziggy.parser import Parser, parse
