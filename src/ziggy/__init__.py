5498daaae302c453b041677201a9aa8728cd39f6
Louis Vignoli <louis.vignoli@gmail.com>
2025-10-27 13:32:58 +0100

    Setup documentation generation

--- src/ziggy/__init__.py
+++ src/ziggy/__init__.py
@@ -2,6 +2,9 @@
 
 Leverage tree-sitter and the Ziggy tree-sitter grammar.
 Ziggy schema is not supported.
+
+This is work in progress, especially the public API of the package, that is a
+heterogeneous.
 """
 
 from ziggy.parser import Parser, parse
